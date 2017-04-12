335accf85665046feccf055b5a572569a148032a
FernandoSolano <fer_odi10@hotmail.com>
2017-04-12 12:30:19 -0600

    Uploaded project

---
+++ src/main/java/com/bibliotecaParaiso/prestamos/BibliotecaParaisoApplication.java
@@ -0,0 +1,12 @@
+package com.bibliotecaParaiso.prestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaParaisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaParaisoApplication.class, args);
+	}
+}
